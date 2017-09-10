13ff8a610a039a48fd4113d5223a4bb77ad3a940
Jaquelini <Jaque@MacBook-Air-de-Gustavo-Barcellos-De-Miranda.local>
2017-09-10 19:25:25 -0300

    Classe SistemaDePedidos

---
+++ src/main/java/br/com/senac/restaurante/SistemaDePedidos.java
@@ -0,0 +1,11 @@
+package br.com.senac.restaurante;
+
+public class SistemaDePedidos {
+
+	public static void main(String[] args) {
+		Restaurante restaurante = new Restaurante();
+
+		restaurante.mostrarCardapio();
+	}
+
+}
